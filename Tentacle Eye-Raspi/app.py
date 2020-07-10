9ca0c455816ee3adab9334485065e01407188651
INTJ <55939719+Logic-01001010@users.noreply.github.com>
2020-07-10 11:23:40 +0900

    Add files via upload

--- Tentacle Eye-Raspi/app.py
+++ Tentacle Eye-Raspi/app.py
@@ -19,6 +19,9 @@ def limit_remote_addr():
 
         try:
             a=1
+            #url = 'http://ic.deexint.com/WhiteList.txt'
+            #r = requests.get(url, allow_redirects=True)
+            #open('WhiteList.txt', 'wb').write(r.content)
 
         except:
             print("Server Down")
--- Tentacle Eye-Raspi/camera.py
+++ Tentacle Eye-Raspi/camera.py
@@ -1,31 +1,29 @@
 from threading import Thread, Lock
 import cv2
 import time
+from datetime import datetime
 
 import os
 import shutil
-
-
-
-
 
 
 class CameraStream(object):
     def __init__(self, src=0):
         self.stream = cv2.VideoCapture(src)
 
-
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        
+        
         
 
         (self.grabbed, self.frame) = self.stream.read()
         
-
+        
         
         self.started = False
         self.read_lock = Lock()
 
     def start(self):
+        
         if self.started:
             print("already started!!")
             return None
@@ -35,6 +33,9 @@ class CameraStream(object):
         return self
 
     def update(self):
+        #self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+        #self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+        
         while self.started:
             (grabbed, frame) = self.stream.read()
             self.read_lock.acquire()
@@ -42,15 +43,18 @@ class CameraStream(object):
             self.read_lock.release()
 
     def read(self):
+        
+        
         self.read_lock.acquire()
         frame = self.frame.copy()
         self.read_lock.release()
 
         frame = cv2.flip(frame, 0)
+        frame = cv2.flip(frame, 1)
         
 
-        cv2.putText(frame, text=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), org=(30, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0,255,0), thickness=2)
-        
+        cv2.putText(frame, text=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'.'+str(int(datetime.utcnow().strftime('%f'))/10000), org=(30, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(0,0,0), thickness=5)
+        cv2.putText(frame, text=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+'.'+str(int(datetime.utcnow().strftime('%f'))/10000), org=(30, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(255,255,255), thickness=2)       
         return frame
 
     def stop(self):
